ba9e5cde0050dae04c34228e53d38f0997ac4214
Alex Lychak <lishkon@gmail.com>
2020-11-03 16:41:32 +0200

    Quadratic equation changes

--- Problems/Quadratic equation/src/Main.java
+++ Problems/Quadratic equation/src/Main.java
@@ -7,9 +7,9 @@ class Main {
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
-        double root = Math.sqrt(Math.pow(b,2) - 4 * a * c);
-        double firstRoot = (-b + root ) / (2 * a);
-        double secondRoot = (-b - root ) / (2 * a);
+        double root = Math.sqrt(Math.pow(b, 2) - 4 * a * c);
+        double firstRoot = (-b + root) / (2 * a);
+        double secondRoot = (-b - root) / (2 * a);
         if (firstRoot < secondRoot) {
             System.out.println(firstRoot + " " + secondRoot);
         } else {
