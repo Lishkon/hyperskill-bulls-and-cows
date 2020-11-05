d1ebda1312c0558c4aec48603ce5583b0e5960ae
Alex Lychak <lishkon@gmail.com>
2020-11-05 09:39:08 +0200

    reuploading the files

---
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -0,0 +1,125 @@
+package bullscows;
+
+import java.util.*;
+import java.util.jar.JarOutputStream;
+
+public class Main {
+    public static void main(String[] args) {
+//        String code = generateCode();
+//        String answer = "";
+//        String grade = "";
+//
+//        int attepmptCounter = 0;
+//        boolean stop = false;
+//        Main main = new Main();
+//
+//        // Prompt the user
+        Scanner scanner = new Scanner(System.in);
+        generateCode(scanner.nextInt());
+//        System.out.println("The secret code is prepared: ****.");
+//
+//        while (!stop) {
+//            attepmptCounter++;
+//
+//            System.out.print("Turn " + attepmptCounter + ". Answer: ");
+//            answer = scanner.nextLine();
+//            grade = main.gradeProcessor(code, answer);
+//
+//            if (grade.equals("4 bull(s)")) {
+//                System.out.println("Congrats! The secret code is: " + code);
+//                stop = true;
+//            } else {
+//                System.out.println("Grade: " + grade + ". The secret code is " + code);
+//            }
+//        }
+
+//        generateCode(scanner.nextInt());
+//        System.out.println(modulize(scanner.nextInt()));
+
+    }
+
+    /**
+     * Generates a random 4-digit long code with unique numbers
+     * @return String
+     */
+    public static String generateCode() {
+        Integer[] arr = {0,1,2,3,4,5,6,7,8,9};
+        Collections.shuffle(Arrays.asList(arr));
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            result.append(arr[i]);
+        }
+        return result.toString();
+    }
+
+
+    /**
+     * Generates a random code of a given length with unique numbers
+     * @param length
+     * @return String
+     */
+    public static void generateCode(int length) {
+        Long randomNumber = System.nanoTime();
+        String numToString = String.valueOf(randomNumber);
+
+        StringBuilder result = new StringBuilder();
+        for (int i = numToString.length()-1; i > 0; i--) {
+            for (int j = 0; j < length; j++) {
+                if (isUniqueChar(result.charAt(j), numToString)) {
+                    result.append(numToString.charAt(i));
+                } else {
+                    return;
+                }
+            }
+        }
+        System.out.println(result);
+
+    }
+
+    public static boolean isUniqueChar(char ch, String token) {
+        return token.contains(String.valueOf(ch));
+    }
+
+    public String gradeProcessor(String code, String answer) {
+        int bullCounter = 0;
+        int cowCounter = 0;
+        String result = "None";
+
+        // Arranging the codeArray
+        int[] codeArray = new int[code.length()];
+        for (int i = 0; i < code.length(); i++) {
+            codeArray[i] = Character.getNumericValue(code.charAt(i));
+        }
+
+        // Arranging the cowArray
+        int[] answerArray = new int[code.length()];
+        for (int i = 0; i < code.length(); i++) {
+            answerArray[i] = Character.getNumericValue(answer.charAt(i));
+        }
+
+        // Counting the bulls and cows
+        if (code.equals(answer)) {
+            bullCounter = 4;
+        } else {
+            for (int i = 0; i < answerArray.length; i++) {
+                for (int j = 0; j < codeArray.length; j++) {
+                    if (answerArray[i] == codeArray[j] && i==j) {
+                        bullCounter++;
+                    } else if (answerArray[i] == codeArray[j]) {
+                        cowCounter++;
+                    }
+                }
+            }
+        }
+
+        if (bullCounter != 0 && cowCounter == 0) {
+            result = bullCounter + " bull(s)";
+        } else if (bullCounter == 0 && cowCounter != 0) {
+            result = cowCounter + " cow(s)";
+        } else if (bullCounter != 0 && cowCounter != 0) {
+            result = bullCounter + " bull(s) and " + cowCounter + " cow(s)";
+        }
+
+        return result;
+    }
+}
